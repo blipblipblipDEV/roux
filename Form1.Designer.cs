4e5cc4c374b9cc6267a33815cfa988a875eb0eea
blipblipblipDEV <66701157+blipblipblipDEV@users.noreply.github.com>
2020-09-24 17:24:02 -0400

    Added Toolbars, Buttons, and improvments.

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,10 +36,17 @@
             this.btnMakeRoux = new System.Windows.Forms.Button();
             this.txtConsole = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.visit = new System.Windows.Forms.Button();
+            this.clear = new System.Windows.Forms.Button();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.getRouxToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fortniteIsCringeTiktokIsCringierToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             ((System.ComponentModel.ISupportInitialize)(this.numRoux)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // txtUsername
@@ -92,7 +99,7 @@
             // btnMakeRoux
             // 
             this.btnMakeRoux.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnMakeRoux.Location = new System.Drawing.Point(29, 159);
+            this.btnMakeRoux.Location = new System.Drawing.Point(64, 158);
             this.btnMakeRoux.Name = "btnMakeRoux";
             this.btnMakeRoux.Size = new System.Drawing.Size(205, 96);
             this.btnMakeRoux.TabIndex = 4;
@@ -104,9 +111,10 @@
             // 
             this.txtConsole.BackColor = System.Drawing.Color.Black;
             this.txtConsole.ForeColor = System.Drawing.Color.Lime;
-            this.txtConsole.Location = new System.Drawing.Point(388, 77);
+            this.txtConsole.Location = new System.Drawing.Point(388, 87);
             this.txtConsole.Multiline = true;
             this.txtConsole.Name = "txtConsole";
+            this.txtConsole.ReadOnly = true;
             this.txtConsole.Size = new System.Drawing.Size(470, 333);
             this.txtConsole.TabIndex = 5;
             this.txtConsole.TextChanged += new System.EventHandler(this.txtConsole_TextChanged);
@@ -114,32 +122,102 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(385, 23);
+            this.label1.Location = new System.Drawing.Point(615, 32);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(431, 17);
+            this.label1.Size = new System.Drawing.Size(59, 17);
             this.label1.TabIndex = 6;
-            this.label1.Text = "Dev Console (NOTE: This is where it tells you the process is done.)";
+            this.label1.Text = "Console";
             this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // openFileDialog1
-            // 
-            this.openFileDialog1.FileName = "openFileDialog1";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(388, 43);
+            this.pictureBox1.Location = new System.Drawing.Point(388, 52);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(491, 38);
             this.pictureBox1.TabIndex = 7;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
+            // visit
+            // 
+            this.visit.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.visit.Location = new System.Drawing.Point(181, 364);
+            this.visit.Name = "visit";
+            this.visit.Size = new System.Drawing.Size(174, 74);
+            this.visit.TabIndex = 10;
+            this.visit.Text = "Visit ROBLOX";
+            this.visit.UseVisualStyleBackColor = true;
+            this.visit.Click += new System.EventHandler(this.visit_Click);
+            // 
+            // clear
+            // 
+            this.clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clear.Location = new System.Drawing.Point(12, 364);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(147, 74);
+            this.clear.TabIndex = 8;
+            this.clear.Text = "Clear Console";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(29, 28);
+            this.toolStripButton1.Text = "toolStripButton1";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.getRouxToolStripMenuItem,
+            this.fortniteIsCringeTiktokIsCringierToolStripMenuItem});
+            this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(34, 24);
+            this.toolStripDropDownButton1.Text = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Click += new System.EventHandler(this.toolStripDropDownButton1_Click);
+            // 
+            // getRouxToolStripMenuItem
+            // 
+            this.getRouxToolStripMenuItem.Name = "getRouxToolStripMenuItem";
+            this.getRouxToolStripMenuItem.Size = new System.Drawing.Size(315, 26);
+            this.getRouxToolStripMenuItem.Text = "Get Secret Roux";
+            this.getRouxToolStripMenuItem.Click += new System.EventHandler(this.getRouxToolStripMenuItem_Click);
+            // 
+            // fortniteIsCringeTiktokIsCringierToolStripMenuItem
+            // 
+            this.fortniteIsCringeTiktokIsCringierToolStripMenuItem.Name = "fortniteIsCringeTiktokIsCringierToolStripMenuItem";
+            this.fortniteIsCringeTiktokIsCringierToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.fortniteIsCringeTiktokIsCringierToolStripMenuItem.Text = "Get V-Bucks";
+            this.fortniteIsCringeTiktokIsCringierToolStripMenuItem.Click += new System.EventHandler(this.fortniteIsCringeTiktokIsCringierToolStripMenuItem_Click);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripDropDownButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(907, 27);
+            this.toolStrip1.TabIndex = 11;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(946, 450);
+            this.ClientSize = new System.Drawing.Size(907, 547);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.visit);
+            this.Controls.Add(this.clear);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtConsole);
@@ -154,6 +232,8 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.numRoux)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -168,8 +248,14 @@
         private System.Windows.Forms.Button btnMakeRoux;
         private System.Windows.Forms.TextBox txtConsole;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.OpenFileDialog openFileDialog1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button visit;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripMenuItem getRouxToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem fortniteIsCringeTiktokIsCringierToolStripMenuItem;
+        private System.Windows.Forms.ToolStrip toolStrip1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -45,16 +47,58 @@ namespace roux
 
         private void label1_Click(object sender, EventArgs e)
         {
-            txtConsole.AppendText(string.Format("DONT CLICK THIS LABEL!") + Environment.NewLine);
+            
          
         }
 
         private void txtConsole_TextChanged(object sender, EventArgs e)
         {
-
+            
         }
 
         private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtConsole.Clear();
+        }
+
+        private void close_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void visit_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.roblox.com");
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            txtConsole.AppendText(string.Format("party blob") + Environment.NewLine);
+        }
+
+        private void toolStripDropDownButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void getRouxToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            txtConsole.AppendText(string.Format("Getting Secret ROUX...") + Environment.NewLine);
+            System.Threading.Thread.Sleep(2000);
+            txtConsole.AppendText(string.Format("Done! You now have secret ROUX! (a.k.a. infinite roux)") + Environment.NewLine);
+        }
+
+        private void fortniteIsCringeTiktokIsCringierToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            txtConsole.AppendText(string.Format("ERROR: V-BUCK is not a resource in this current context. Did you mean ROUX-REWARD?") + Environment.NewLine);
+        }
+
+        private void toolStripTextBox1_Click(object sender, EventArgs e)
         {
 
         }
